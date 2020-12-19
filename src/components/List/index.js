17f86e249a7ede9ae1da1b8099532c98b5557a6e
yaco16 <65079827+yaco16@users.noreply.github.com>
2020-12-19 12:15:51 +0100

    upd dynamic lists of items with dynamic MyComponent

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -4,27 +4,27 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 // import PropTypes from 'prop-types';
 
-import { capitalize } from 'src/selectors/functions';
 import AddItem from 'src/components/Modal';
-import Country from 'src/containers/Country';
+import MyComponent from 'src/selectors/MyComponent';
 
 import 'semantic-ui-css/semantic.min.css';
 import 'src/styles/index.scss';
 
 const List = ({ category }) => {
   console.log('category', category);
+
   return (
     <div className="category__container">
       <Link to={{ pathname: '/' }} className="category__nav">
         Retour accueil
       </Link>
       <div className="category__subContainer">
-        <h1 className="category__title">Liste des {category[0].name}</h1>
+        <h1 className="category__title">Liste des {category[0].nameFr}</h1>
         <AddItem item={category[0]} />
       </div>
 
       <ul>
-        <Country />
+        <MyComponent tag={String(category[0].nameSingular)} />
       </ul>
     </div>
   );
