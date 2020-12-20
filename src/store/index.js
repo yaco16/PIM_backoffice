40856e224e0cd55d4bce498ed34f37d315d84005
yaco16 <65079827+yaco16@users.noreply.github.com>
2020-12-20 14:31:35 +0100

    upd store with thunk

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,16 @@
 import { createStore, compose, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
 import countries from '../middlewares/countries';
 import currencies from '../middlewares/currencies';
+
+const middleware = [thunk];
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const enhancers = composeEnhancers(
   applyMiddleware(
+    ...middleware,
     countries,
     currencies,
   ),
