13538dc75b2ab57ed9e000d9f600ceafe968c205
yaco16 <65079827+yaco16@users.noreply.github.com>
2020-12-20 14:32:29 +0100

    add show_countries in countries actions

--- src/actions/countries.js
+++ src/actions/countries.js
@@ -1,5 +1,6 @@
 export const SAVE_COUNTRIES = 'SAVE_COUNTRIES';
 export const FETCH_COUNTRIES = 'FETCH_COUNTRIES';
+export const SHOW_COUNTRIES = 'SHOW_COUNTRIES';
 
 export const saveCountries = (countries) => ({
   type: SAVE_COUNTRIES,
